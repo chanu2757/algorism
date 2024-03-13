0a5ba55420bee8e9a07ff956a273a31f683df3e5
chanu2757 <chanu2757@gmail.com>
2024-03-13 23:40:31 +0900

    20240313 getRadius

---
+++ src/com/java/bangtong/Circle.java
@@ -0,0 +1,25 @@
+package com.java.bangtong;
+
+class Circle{
+    private double r;
+    public Circle(double a){
+        r = a;
+    }
+    public double getArea(){
+        return r*r *3.14;
+    }
+    public double getRadius(){
+        return r;
+    }
+}
+
+class Main{
+    public static void main(String args[]){
+        Circle c = new Circle(5); // 접근제어가 없으므로 같은 패키지내에서 사용가능
+        //System.out.println(c.r); //오류 r은 private이므로 같은 클래스에서만 사용가능
+        System.out.println(c.getRadius());
+        System.out.println(c.getArea());
+    
+    }
+}
+
